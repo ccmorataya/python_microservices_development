f3f7a617adc4c5c73f1dfeb1d3a988aeafe908f4
Cristian Morataya <cris.morataya@gmail.com>
2020-03-12 01:17:24 -0600

    Add URL with path param

--- flask_basic.py
+++ flask_basic.py
@@ -1,10 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/api')
 def my_microservice():
-    return jsonify({'Hello': 'World!'})
+    print('CM:: request', request)
+    print('CM:: request.environ', request.environ)
+    response = jsonify({'Hello': 'World!'})
+    print('CM:: response', response)
+    print('CM:: response.data', response.data)
+    return response
+
+@app.route('/api/person/<person_id>')
+def person(person_id):
+    response = jsonify({'Hello': person_id})
+    return response
 
 if __name__ == '__main__':
+    print('CM:: app.url_map', app.url_map)
     app.run(host='0.0.0.0')
