cc17295089f87a31f9962b2cc1bcd7165fdf46f8
Cristian Morataya <cris.morataya@gmail.com>
2020-03-12 01:32:52 -0600

    Add custom converter map ID with USERS

--- flask_basic.py
+++ flask_basic.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify, request
+from werkzeug.routing import BaseConverter, ValidationError
+
+_USERS = {'1': 'Tarek', '2': 'Freya'}
+_IDS = {val: id for id, val in _USERS.items()}
+
+class RegisteredUser(BaseConverter):
+    def to_python(self, value):
+        if value in _USERS:
+            return _USERS[value]
+        raise ValidationError()
+    def to_url(self, value):
+        return _IDS[value]
 
 app = Flask(__name__)
+app.url_map.converters['registered'] = RegisteredUser
 
 @app.route('/api')
 def my_microservice():
@@ -11,9 +24,14 @@ def my_microservice():
     print('CM:: response.data', response.data)
     return response
 
-@app.route('/api/person/<person_id>')
-def person(person_id):
-    response = jsonify({'Hello': person_id})
+# @app.route('/api/person/<person_id>')
+# def person(person_id):
+#     response = jsonify({'Hello': person_id})
+#     return response
+
+@app.route('/api/person/<registered:name>')
+def person(name):
+    response = jsonify({'Hello hey': name})
     return response
 
 if __name__ == '__main__':
